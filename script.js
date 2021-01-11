9202a54a30c364a68ab388d3f9ff5f120674daba
fenfil <vladimir.zaykovskiy@ya.ru>
2021-01-11 22:22:56 +0300

    fix

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const headless = +process.env.HEADLESS;
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
 
